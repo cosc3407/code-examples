a66b9c991b6c1c72317600f3f53d0f4feb97f944
pagzamaphone <anthony.pagnotta@gmail.com>
2023-11-05 23:19:40 -0500

    Create Main.java

---
+++ tutorial4/Counting Semaphore Example/Main.java
@@ -0,0 +1,15 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+	public static void main(String[] args) {
+		final int threadCount = 10;
+		final ExecutorService exService = Executors.newFixedThreadPool(threadCount);
+		final Library library = new Library();
+		for(int i=0; i < threadCount; i++) {
+			Reader reader = new Reader(library);
+			exService.execute(reader);
+		}
+		exService.shutdown();
+	}
+}
