8f953006b6d855a9f16e72457d826df71b2b0281
pagzamaphone <anthony.pagnotta@gmail.com>
2023-11-05 23:20:08 -0500

    Create Reader.java

---
+++ tutorial4/Counting Semaphore Example/Reader.java
@@ -0,0 +1,16 @@
+public class Reader implements Runnable {
+	private Library library;
+	public Reader(Library library) {
+		this.library = library;
+	}
+	@Override
+	public void run() {
+		try {
+			Book book = (Book)library.issueBook();
+			book.read();
+			library.returnBook(book);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
