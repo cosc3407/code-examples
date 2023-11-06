6d9a8913705dbaeaf77ac1815d46982b67953a1c
pagzamaphone <anthony.pagnotta@gmail.com>
2023-11-05 23:18:53 -0500

    Create Book.java

---
+++ tutorial4/Counting Semaphore Example/Book.java
@@ -0,0 +1,17 @@
+public class Book {
+	private String bookName;
+	public Book(String bookName) {
+		this.bookName = bookName;
+	}
+	public void read() {
+		System.out.println(bookName + " is being read......");
+		try {
+			Thread.sleep(2000);
+		}catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	public String getBookName() {
+		return bookName;
+	}
+}
